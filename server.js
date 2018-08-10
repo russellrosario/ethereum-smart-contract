3ec85fd5da55ab9d9a79d328111332ac68dcf4b1
Russell Rosario <rosariorussell@gmail.com>
2018-08-10 13:14:06 -0400

    updated port

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-const { createServer } = require('http');
-const next = require('next');
+const { createServer } = require('http')
+const next = require('next')
 
 const app = next({
   dev: process.env.NODE_ENV !== 'production',
   conf: {
     webpack: config => {
-      config.devtool = false;
+      config.devtool = false
 
       for (const r of config.module.rules) {
         if (r.loader === 'babel-loader') {
-          r.options.sourceMaps = false;
+          r.options.sourceMaps = false
         }
       }
 
-      return config;
+      return config
     }
   }
-});
+})
 
-const routes = require('./routes');
-const handler = routes.getRequestHandler(app);
+const routes = require('./routes')
+const handler = routes.getRequestHandler(app)
+
+const PORT = process.env.PORT || 3000
 
 app.prepare().then(() => {
-  createServer(handler).listen(3000, err => {
-    if (err) throw err;
-    console.log('Ready on localhost:3000');
-  });
-});
+  createServer(handler).listen(PORT, err => {
+    if (err) throw err
+    console.log('Ready on localhost:3000')
+  })
+})
